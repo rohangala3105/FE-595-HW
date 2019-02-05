a156d58a6a0b8938ad4cf341f054b3be5f980ecf
Poojan Gajera <38298632+poojan1gajera@users.noreply.github.com>
2019-02-05 12:52:30 -0500

    Add files via upload

---
+++ 10432734_HW_1_FE_595.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+'''
+Name:       Poojan Gajera
+Stevens ID: 10432734
+Course:     FE 595
+HW:         01
+Title:      Python Refresher
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Taking values from 0 to 360 for the cycle of sine/cosine.
+period = np.arange(0,2*np.pi,0.01)
+
+# Sine and Cosine Graph
+sine = np.sin(period )
+cosine = np.cos(period )
+
+# Creating plots, X & Y axes and legends
+plt.plot(period , sine , period , cosine)
+
+plt.subplot().legend(['Sine','Cosine'])               #
+
+plt.subplot().axhline(y=0, color='k')
+plt.subplot().axvline(x=0, color='k')
+
+plt.show()
